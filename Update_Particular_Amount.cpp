9c3e88f3e73208ed3e8689aa73231a6241d4985f
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-06-05 12:58:44 +0530

    Implemented deposit feature with account balance update.

---
+++ Update_Particular_Amount.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+    int AccNo;
+    char Name[25], Fname[25], cnic[25], P_No[25], Email[25], Address[25];
+    float Amount;
+    fstream file, file1;
+
+    int SearchACC;
+    float newAmount;
+    bool Found = false;
+
+    cout << "Enter Your Account Number :: ";
+    cin >> SearchACC;
+    cout << "Enter Your Amount to Deposit :: ";
+    cin >> newAmount;
+
+    file.open("dataSave4.txt", ios::in);
+    file1.open("dataSave5.txt", ios::out);
+
+    if (!file)
+    {
+        cout << "File Not Found";
+        return 0;
+    }
+
+    while (file >> AccNo >> Name >> Fname >> cnic >> P_No >> Email >> Address >> Amount)
+    {
+        if (AccNo == SearchACC)
+        {
+            cout << "Your Amount Before Deposition :: " << Amount;
+            Amount += newAmount;
+            cout << "\nYour Current Amount         :: " << Amount;
+
+            cout << "\n------------------------------------------------------SUMMARY YOUR ACCOUNT-----------------------------------------------\n";
+            cout << "\nAccount Number::" << AccNo;
+            cout << "\nName          ::" << Name;
+            cout << "\nFather's Name ::" << Fname;
+            cout << "\nCNIC          ::" << cnic;
+            cout << "\nPhone Number  ::" << P_No;
+            cout << "\nEmail         ::" << Email;
+            cout << "\nAddress       ::" << Address;
+            cout << "\nAmount        ::" << Amount;
+
+            Found = true;
+        }
+
+        file1 << AccNo << "\t" << Name << "\t" << Fname << "\t" << cnic << "\t"
+              << P_No << "\t" << Email << "\t" << Address << "\t" << Amount << endl;
+    }
+
+    file.close();
+    file1.close();
+
+    if (!Found)
+    {
+        cout << "Invalid Account Number.....";
+    }
+    remove("dataSave4.txt");
+    rename("dataSave5.txt", "dataSave4.txt");
+
+    return 0;
+}
